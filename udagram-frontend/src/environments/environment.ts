73d93d33267c2f55d2ebf52501c726936f6d83fb
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-11 19:41:53 +0100

    end of k8s part in project 3

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ac12d195ed58c4f52bd060912936cb43-1161645129.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
